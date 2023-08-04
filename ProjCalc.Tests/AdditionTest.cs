8796b067095504bd5bc02a999f9786cb756c5a41
Akila <devakila001@DESKTOP-VQI9MT1>
2023-08-04 19:20:18 +0530

    Created new ProjCalc and ProjCalc.Tests

---
+++ ProjCalc.Tests/AdditionTest.cs
@@ -0,0 +1,16 @@
+using ProjCalc;
+namespace ProjCalc.Tests;
+
+public class AdditionTest
+{
+    [Fact]
+    public void AddTwoSingleDigitTest()
+    {
+        // Arrange
+        Calculator myCalc = new Calculator();
+        //Act
+
+        //Assert 
+
+    }
+}
